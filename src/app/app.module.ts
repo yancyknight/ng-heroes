583684eb3d7c1946e4cae2f312659d1aeedfb993
Yancy Knight <yancy.knight@gmail.com>
2017-03-21 12:57:43 -0600

    Finished http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
-
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
-
+import { HeroSearchComponent }  from './hero-search.component';
 @NgModule({
   imports: [
     BrowserModule,
@@ -28,6 +25,7 @@ import { HeroService }          from './hero.service';
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
